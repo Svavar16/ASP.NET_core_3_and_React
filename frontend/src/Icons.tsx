ba269090b8b833bfcded67ea2c3962ed167c005d
Svavar Pall Gudgeirsson <spg009@gmail.com>
2020-05-24 11:47:06 +0000

    adding css to the frontend

--- frontend/src/Icons.tsx
+++ frontend/src/Icons.tsx
@@ -1,4 +1,14 @@
 import React from "react";
+import { css, jsx } from "@emotion/core";
 import user from "./zondicons/zondicons/user.svg";
 
-export const UserIcon = () => <img src={user} alt="User" width="12px" />;
+export const UserIcon = () => (
+	<img
+		src={user}
+		alt="User"
+		css={css`
+			width: 12px;
+			opacity: 0.6;
+		`}
+	/>
+);
